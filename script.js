d6dd7308f05cbba96f78b531093afc5505488e56
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-17 22:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,64 +1,77 @@
 //your JS code here. If required.
 const app = document.getElementById('app');
-const videoPlayer = document.getElementById('videoPlayer');
-const audioPlayer = document.getElementById('audioPlayer');
-const soundPicker = document.querySelector('.sound-picker');
+const videoContainer = document.querySelector('.vid-container');
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+const soundButtons = document.querySelectorAll('.sound-picker button');
 const timeButtons = document.querySelectorAll('.time-select button');
 const timeDisplay = document.querySelector('.time-display');
-const playPauseBtn = document.getElementById('playPauseBtn');
+const playButton = document.querySelector('.play');
 
-let selectedSound = 'Sounds/beach.mp3';
-let selectedVideo = 'Videos/beach.mp4';
+let currentTime = 10 * 60; // 10 minutes in seconds
+let isPlaying = false;
 
-function switchSound(event) {
-  const soundButtons = soundPicker.querySelectorAll('button');
-  soundButtons.forEach(button => button.classList.remove('selected'));
-  event.target.classList.add('selected');
-
-  if (event.target.id === 'sound1') {
-    selectedSound = 'Sounds/beach.mp3';
-    selectedVideo = 'Videos/beach.mp4';
-  } else if (event.target.id === 'sound2') {
-    selectedSound = 'Sounds/rain.mp3';
-    selectedVideo = 'Videos/rain.mp4';
-  }
-
-  audioPlayer.src = selectedSound;
-  videoPlayer.src = selectedVideo;
+function updateTimer() {
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = currentTime % 60;
+  timeDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
 }
 
-function switchTime(event) {
-  timeButtons.forEach(button => button.classList.remove('active'));
-  event.target.classList.add('active');
+function startTimer(duration) {
+  currentTime = duration * 60;
+  updateTimer();
 
-  const time = event.target.innerText.split(' ')[0];
-  const minutes = parseInt(time);
-  const seconds = minutes * 60;
-  setTimeDisplay(seconds);
+  const timer = setInterval(() => {
+    currentTime--;
+    updateTimer();
+
+    if (currentTime <= 0) {
+      clearInterval(timer);
+      pauseMeditation();
+    }
+  }, 1000);
 }
 
-function setTimeDisplay(totalSeconds) {
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-  timeDisplay.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+function pauseMeditation() {
+  isPlaying = false;
+  video.pause();
+  audio.pause();
+  playButton.textContent = 'Play';
 }
 
-function playPause() {
-  if (audioPlayer.paused) {
-    audioPlayer.play();
-    videoPlayer.play();
-    playPauseBtn.classList.remove('play');
-    playPauseBtn.classList.add('pause');
-    playPauseBtn.innerText = 'Pause';
-  } else {
-    audioPlayer.pause();
-    videoPlayer.pause();
-    playPauseBtn.classList.remove('pause');
-    playPauseBtn.classList.add('play');
-    playPauseBtn.innerText = 'Play';
-  }
+function playMeditation() {
+  isPlaying = true;
+  video.play();
+  audio.play();
+  playButton.textContent = 'Pause';
 }
 
-soundPicker.addEventListener('click', switchSound);
-timeButtons.forEach(button => button.addEventListener('click', switchTime));
-playPauseBtn.addEventListener('click', playPause);
+soundButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const soundFile = button.id === 'sound1' ? 'beach.mp3' : 'rain.mp3';
+    audio.src = `sounds/${soundFile}`;
+	  audio.play();
+
+// Update the active button styling
+soundButtons.forEach(btn => btn.classList.remove('active'));
+button.classList.add('active');
+});
+});
+
+timeButtons.forEach(button => {
+button.addEventListener('click', () => {
+const time = button.id === 'smaller-mins' ? 2 : button.id === 'medium-mins' ? 5 : 10;
+startTimer(time);
+// Update the active button styling
+timeButtons.forEach(btn => btn.classList.remove('active'));
+button.classList.add('active');
+});
+});
+
+playButton.addEventListener('click', () => {
+if (isPlaying) {
+pauseMeditation();
+} else {
+playMeditation();
+}
+});
